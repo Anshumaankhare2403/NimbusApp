c673e4b736a0961e77293391dbd99dfeabaf39fc
Anshumaankhare2403 <anshumaankhare2403@gmail.com>
2026-07-16 11:56:27 +0530

    numbus

--- frontend/Nimbus/src/App.jsx
+++ frontend/Nimbus/src/App.jsx
@@ -1,9 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 
+import HomePage from "./pages/HomePage.jsx";
+import MoviesPage from "./pages/MoviesPage.jsx";
 function App() {
   return (
-    <div>
-      home 
-    </div>
+    <>
+       <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movies" element={<MoviesPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+        </Routes>
+    </>
   )
 }
 
